f4b8a4e434c2c14d320a410314f8ab79517a8cda
GabrielCosta01 <gabrielcorreacostagt@gmail.com>
2025-05-07 18:47:30 -0300

    feat: Created features of the safes entity

---
+++ src/app/safes/layout.tsx
@@ -0,0 +1,14 @@
+'use client';
+import ClientLayout from '@/components/ClientLayout';
+
+export default function SafesLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClientLayout>
+      {children}
+    </ClientLayout>
+  );
+} 
